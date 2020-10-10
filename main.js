5a6e65741c8385f0d80d8bfd4653f398c42d28b1
David <dhahn123@gmail.com>
2020-10-09 19:34:38 -0700

    create saveIssues function, pass function into body tag

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    var issueId = chance.guid(); // returns global unique identifier and assigns identifier
+    var issueStatus = 'Open';
+
+    var issue = {
+        id: issueId,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    if (localStorage.getItem('issues') === null) {
+        var issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+        var issues = JSON.parse(localStorage.getItem('issues'));
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    }
+
+    document.getElementById('issueInputForm').reset();
+
+    fetchIssue();
+
+    e.preventDefault();
+}
+
 function fetchIssue() {
     var issues = JSON.parse(localStorage.getItem('issues'));
     var issuesList = document.getElementById('issuesList');
